9ec2d552fb63d2687ed1d1012d3a81198059e810
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-06-05 10:11:01 +0300

    fix-img-next

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // إذا كنت تستخدم صوراً خارجية (مثل Unsplash أو أي استضافة خارجية)
-    
-    // إذا كنت تستخدم صوراً محلية (مخزنة في `public/`)
-    unoptimized: true, // (حل بديل إذا كانت الصور المحلية لا تعمل)
-    
-    // إذا كنت تريد السماح بجميع النطاقات (غير موصى به للأمان)
-    // dangerouslyAllowSVG: true,
-    // contentDispositionType: 'attachment',
-    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
